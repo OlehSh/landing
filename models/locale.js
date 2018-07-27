6af2a96d91d8e3caf20e29d68a5b3bc5eed23c10
olehsh <shubinoleh@gmail.com>
2018-07-27 08:48:15 +0300

    update layout and dump

--- models/locale.js
+++ models/locale.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let localSchema = new Schema({
+    language:  String,
     title:  String,
     article: String,
+    menu: [String]
 });
 
 const Locale = mongoose.model('Locale', localSchema);
